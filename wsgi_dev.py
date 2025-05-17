b4fa0155ecf7fdf7e288807acac638673726a879
Jonas Bunsen <83568124+jbnsn@users.noreply.github.com>
2025-05-17 09:07:23 +0200

    bang

---
+++ wsgi_dev.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 17 08:59:21 2025
+
+@author: jonas
+"""
+
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
